cf0efd082ef883266e043fb4cabf632069c95f12
yuvipanda <yuvipanda@gmail.com>
2017-12-15 14:15:15 -0800

    Refactor to be more testable + add tests

--- ltiauthenticator/__init__.py
+++ ltiauthenticator/__init__.py
@@ -10,27 +10,91 @@ from jupyterhub.utils import url_path_join
 from oauthlib.oauth1.rfc5849 import signature
 from collections import OrderedDict
 
+class LTILaunchValidator:
+    # Record time when process starts, so we can reject requests made
+    # before this
+    PROCESS_START_TIME = int(time.time())
 
-PROCESS_START_TIME = time.time()
+    # Keep a class-wide, global list of nonces so we can detect & reject
+    # replay attacks. This possibly makes this non-threadsafe, however.
+    nonces = OrderedDict()
 
-nonces = OrderedDict()
+    def __init__(self, consumers):
+        self.consumers = consumers
+
+    def validate_launch_request(
+            self,
+            launch_url,
+            headers,
+            args
+    ):
+        """
+        Validate a given launch request
+
+        launch_url: Full URL that the launch request was POSTed to
+        headers: k/v pair of HTTP headers coming in with the POST
+        args: dictionary of body arguments passed to the launch_url
+            Must have the following keys to be valid:
+                oauth_consumer_key, oauth_timestamp, oauth_nonce,
+                oauth_signature
+        """
+
+        # Validate args!
+        if 'oauth_consumer_key' not in args:
+            raise web.HTTPError(401, "oauth_consumer_key missing")
+        if args['oauth_consumer_key'] not in self.consumers:
+            raise web.HTTPError(401, "oauth_consumer_key not known")
+
+        if 'oauth_signature' not in args:
+            raise web.HTTPError(401, "oauth_signature missing")
+        if 'oauth_timestamp' not in args:
+            raise web.HTTPError(401, 'oauth_timestamp missing')
+
+        # Allow 30s clock skew between LTI Consumer and Provider
+        # Also don't accept timestamps from before our process started, since that could be
+        # a replay attack - we won't have nonce lists from back then. This would allow users
+        # who can control / know when our process restarts to trivially do replay attacks.
+        oauth_timestamp = int(float(args['oauth_timestamp']))
+        if (
+                int(time.time()) - oauth_timestamp > 30
+                or oauth_timestamp < LTILaunchValidator.PROCESS_START_TIME
+        ):
+            raise web.HTTPError(401, "oauth_timestamp too old")
+
+        if 'oauth_nonce' not in args:
+            raise web.HTTPError(401, 'oauth_nonce missing')
+        if (
+                oauth_timestamp in LTILaunchValidator.nonces
+                and args['oauth_nonce'] in LTILaunchValidator.nonces[oauth_timestamp]
+        ):
+            raise web.HTTPError(401, "oauth_nonce + oauth_timestamp already used")
+        LTILaunchValidator.nonces.setdefault(oauth_timestamp, set()).add(args['oauth_nonce'])
 
 
-class LTIAuthenticateHandler(BaseHandler):
-    """
-    Handler for /lti/launch
+        args_list = []
+        for key, values in args.items():
+            if type(values) is list:
+                args_list += [(key, value) for value in values]
+            else:
+                args_list.append((key, values))
 
-    Implements v1 of the LTI protocol for passing authentication information
-    through.
+        base_string = signature.construct_base_string(
+            'POST',
+            signature.normalize_base_string_uri(launch_url),
+            signature.normalize_parameters(
+                signature.collect_parameters(body=args_list, headers=headers)
+            )
+        )
 
-    If there's a custom parameter called 'next', will redirect user to
-    that URL after authentication. Else, will send them to /home.
-    """
+        consumer_secret = self.consumers[args['oauth_consumer_key']]
 
-    @gen.coroutine
-    def post(self):
-        user = yield self.login_user()
-        self.redirect(self.get_body_argument('custom_next', self.get_next_url()))
+        sign = signature.sign_hmac_sha1(base_string, consumer_secret, None)
+        is_valid = signature.safe_string_equals(sign, args['oauth_signature'])
+
+        if not is_valid:
+            raise web.HTTPError(401, "Invalid oauth_signature")
+
+        return True
 
 
 class LTIAuthenticator(Authenticator):
@@ -57,64 +121,44 @@ class LTIAuthenticator(Authenticator):
             ('/lti/launch', LTIAuthenticateHandler)
         ]
 
+
     @gen.coroutine
     def authenticate(self, handler, data=None):
         # FIXME: Run a process that cleans up old nonces every other minute
-        # Validate that we have a consumer key for this request
-        consumer_key = handler.get_body_argument('oauth_consumer_key')
-        if consumer_key not in self.consumers:
-            # Be careful to not reflect the consumer_key here
-            raise web.HTTPError(401, "Invalid oauth_consumer_key")
+        validator = LTILaunchValidator(self.consumers)
+
+        args = {}
+        for k, values in handler.request.body_arguments.items():
+            args[k] = values[0].decode() if len(values) == 1 else [v.decode() for v in values]
 
 
-        # Convert args from dict to list of tuples that oauthlib wants
-        args = []
-        for key, values in handler.request.arguments.items():
-            for value in values:
-                args.append((key, value))
-        base_string = signature.construct_base_string(
-            'POST',
-            signature.normalize_base_string_uri(handler.request.full_url()),
-            signature.normalize_parameters(
-                signature.collect_parameters(
-                    body=args,
-                    headers=handler.request.headers
-                )
-            )
-        )
+        if validator.validate_launch_request(
+                handler.request.full_url(),
+                handler.request.headers,
+                args
+        ):
+            return {
+                'name': handler.get_body_argument('user_id'),
+                'auth_state': {k: v for k, v in args.items() if not k.startswith('oauth_')}
+            }
 
-        consumer_secret = self.consumers[consumer_key]
-
-        sign = signature.sign_hmac_sha1(base_string, consumer_secret, None)
-        is_valid = signature.safe_string_equals(sign, handler.get_body_argument('oauth_signature'))
-
-        if not is_valid:
-            raise web.HTTPError(401, "Invalid oauth_signature")
-
-        ts = int(handler.get_body_argument('oauth_timestamp'))
-        nonce = handler.get_body_argument('oauth_nonce')
-        # Allow 30s clock skew between LTI Consumer and Provider
-        # Also don't accept timestamps from before our process started, since that could be
-        # a replay attack - we won't have nonce lists from back then. This would allow users
-        # who can control / know when our process restarts to trivially do replay attacks.
-        if time.time() - ts > 30 or ts < PROCESS_START_TIME:
-            raise web.HTTPError(401, "oauth_timestamp too old")
-
-        if ts in nonces and nonce in nonces[ts]:
-            raise web.HTTPError(401, "oauth_nonce + oauth_timestamp already used")
-
-        nonces.setdefault(ts, set()).add(nonce)
-
-        auth_state = {}
-        for k, values in handler.request.arguments.items():
-            if not k.startswith('oauth_'):
-                auth_state[k] = values[0].decode() if len(values) == 1 else [v.decode() for v in values]
-
-        print(auth_state)
-        return {
-            'name': handler.get_body_argument('user_id'),
-            'auth_state': auth_state
-        }
 
     def login_url(self, base_url):
         return url_path_join(base_url, '/lti/launch')
+
+
+class LTIAuthenticateHandler(BaseHandler):
+    """
+    Handler for /lti/launch
+
+    Implements v1 of the LTI protocol for passing authentication information
+    through.
+
+    If there's a custom parameter called 'next', will redirect user to
+    that URL after authentication. Else, will send them to /home.
+    """
+
+    @gen.coroutine
+    def post(self):
+        user = yield self.login_user()
+        self.redirect(self.get_body_argument('custom_next', self.get_next_url()))
---
+++ tests/test_lti.py
@@ -0,0 +1,186 @@
+import time
+import pytest
+from tornado import web
+from ltiauthenticator import LTILaunchValidator
+from oauthlib.oauth1.rfc5849 import signature
+
+def make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+):
+    args = {
+        'oauth_consumer_key': consumer_key,
+        'oauth_timestamp': str(oauth_timestamp),
+        'oauth_nonce': oauth_nonce
+    }
+
+    args.update(extra_args)
+
+    base_string = signature.construct_base_string(
+        'POST',
+        signature.normalize_base_string_uri(launch_url),
+        signature.normalize_parameters(
+            signature.collect_parameters(body=args, headers={})
+        )
+    )
+
+    args['oauth_signature'] = signature.sign_hmac_sha1(base_string, consumer_secret, None)
+
+    return args
+
+def test_launch():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({consumer_key: consumer_secret})
+
+    assert validator.validate_launch_request(launch_url, headers, args)
+
+def test_wrong_key():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({'wrongkey': consumer_secret})
+
+    with pytest.raises(web.HTTPError):
+        assert validator.validate_launch_request(launch_url, headers, args)
+
+def test_wrong_secret():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({consumer_key: 'wrongsecret'})
+
+    with pytest.raises(web.HTTPError):
+        validator.validate_launch_request(launch_url, headers, args)
+
+def test_full_replay():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({consumer_key: consumer_secret})
+
+    assert validator.validate_launch_request(launch_url, headers, args)
+
+    with pytest.raises(web.HTTPError):
+        validator.validate_launch_request(launch_url, headers, args)
+
+def test_partial_replay_timestamp():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({consumer_key: consumer_secret})
+
+    assert validator.validate_launch_request(launch_url, headers, args)
+
+    args['oauth_timestamp'] = str(int(float(args['oauth_timestamp'])) - 1)
+    with pytest.raises(web.HTTPError):
+        validator.validate_launch_request(launch_url, headers, args)
+
+def test_partial_replay_nonce():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({consumer_key: consumer_secret})
+
+    assert validator.validate_launch_request(launch_url, headers, args)
+
+    args['oauth_nonce'] = args['oauth_nonce'] + "1"
+    with pytest.raises(web.HTTPError):
+        validator.validate_launch_request(launch_url, headers, args)
+
+def test_dubious_extra_args():
+    consumer_key = 'key1'
+    consumer_secret = 'secret1'
+    launch_url = 'http://localhost:8000/hub/lti/launch'
+    headers = {}
+    oauth_timestamp = time.time()
+    oauth_nonce = str(time.time())
+    extra_args = {
+        'arg1': 'value1'
+    }
+
+    args = make_args(
+        consumer_key, consumer_secret,
+        launch_url, oauth_timestamp, oauth_nonce, extra_args
+    )
+
+    validator = LTILaunchValidator({consumer_key: consumer_secret})
+
+    assert validator.validate_launch_request(launch_url, headers, args)
+
+    args['extra_credential'] = 'i have admin powers'
+    with pytest.raises(web.HTTPError):
+        validator.validate_launch_request(launch_url, headers, args)
