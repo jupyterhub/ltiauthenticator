cf702635e97f68738b4877674d4390c59d5e89ff
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-08-01 15:01:36 -0700

    Bump to version 0.3 for canvas support.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-ltiauthenticator',
-    version='0.2',
+    version='0.3',
     description='JupyterHub authenticator implementing LTI v1',
     url='https://github.com/yuvipanda/jupyterhub-ltiauthenticator',
     author='Yuvi Panda',
