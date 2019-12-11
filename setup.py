86b2e7a00c84b147ee606235e37129e5bf87b01c
Erik Sundell <erik.i.sundell@gmail.com>
2019-12-11 20:28:18 +0100

    Bump version: 0.4.0 → 0.4.1.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-ltiauthenticator',
-    version='0.4.0',
+    version='0.4.1.dev',
     description='JupyterHub authenticator implementing LTI v1',
     url='https://github.com/yuvipanda/jupyterhub-ltiauthenticator',
     author='Yuvi Panda',
