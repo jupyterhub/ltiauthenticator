ee0a6876082a5302f5372ab0f56ef757783119bb
Erik Sundell <erik.i.sundell@gmail.com>
2020-12-04 13:20:39 +0100

    Bump version: 0.4.1.dev → 1.0.0

--- ltiauthenticator/__init__.py
+++ ltiauthenticator/__init__.py
@@ -10,7 +10,7 @@ from jupyterhub.utils import url_path_join
 from oauthlib.oauth1.rfc5849 import signature
 from collections import OrderedDict
 
-__version__ = '0.4.1.dev'
+__version__ = '1.0.0'
 
 class LTILaunchValidator:
     # Record time when process starts, so we can reject requests made
