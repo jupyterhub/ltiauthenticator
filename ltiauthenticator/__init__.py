5c14ec67d188fbdd63fdb181f191efbcee0c3fbf
Greg Werner <werner.greg@gmail.com>
2021-04-08 00:14:12 -0400

    ignore with pyflakes
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- ltiauthenticator/__init__.py
+++ ltiauthenticator/__init__.py
@@ -1,3 +1,3 @@
-from ltiauthenticator.lti11.auth import LTIAuthenticator  # noqa: F401
-from ltiauthenticator.lti11.handlers import LTIAuthenticateHandler  # noqa: F401
-from ltiauthenticator.lti11.validator import LTILaunchValidator  # noqa: F401
+from ltiauthenticator.lti11.auth import LTIAuthenticator  # noqa
+from ltiauthenticator.lti11.handlers import LTIAuthenticateHandler  # noqa
+from ltiauthenticator.lti11.validator import LTILaunchValidator  # noqa
