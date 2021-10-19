ddcc7d7700ce2908f13a3ceb72cb0a675a838c27
jgwerner <werner.greg@gmail.com>
2021-10-19 02:00:03 +0000

    fix remaining tests
    Signed-off-by: jgwerner <werner.greg@gmail.com>

--- ltiauthenticator/tests/test_lti13_validator.py
+++ ltiauthenticator/tests/test_lti13_validator.py
@@ -28,15 +28,6 @@ def test_validate_missing_required_claims_in_step_1_resource_link_request():
         validator.validate_login_request(fake_jws)
 
 
-def test_validate_with_required_params_in_initial_auth_request(make_lti13_login_params):
-    """
-    Is the JWT valid with an correct message type claim?
-    """
-    validator = LTI13LaunchValidator()
-    result = validator.validate_login_request(make_lti13_login_params)
-    assert result is True
-
-
 def test_validate_missing_required_claims_in_step_2_resource_link_request():
     """
     Is the JWT valid with an incorrect message type claim?
