6f466c2c2170a08c604db35f6fb61c8ad34f4efd
Martin Claus <mclaus@geomar.de>
2023-02-16 13:50:34 +0100

    Fix tests

--- tests/lti13/test_lti13_handlers.py
+++ tests/lti13/test_lti13_handlers.py
@@ -5,4 +5,7 @@ async def test_lti_13_handler_paths(app):
     """Test if all handlers are correctly set with the LTI13Authenticator."""
     auth = MockLTI13Authenticator()
     handlers = auth.get_handlers(app)
-    assert handlers[0][0] == "/lti13/config"
+    handler_paths = [route[0] for route in handlers]
+    assert "/lti13/config" in handler_paths
+    assert "/lti13/oauth_login" in handler_paths
+    assert "/lti13/oauth_callback" in handler_paths
